28c0f327143466d4f1149316ab22b8ccdd309cb8
rad <radioactivepb@gmail.com>
2025-04-30 11:36:45 -0500

    Allow network access to app

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(`${__dirname}/${DIST_DIR}`)));
-app.listen(PORT, "localhost", async () => {
+app.listen(PORT, async () => {
   await open(`http://localhost:${PORT}`);
   console.log(`${appName} v${ver} has started on port ${PORT}`);
 });
